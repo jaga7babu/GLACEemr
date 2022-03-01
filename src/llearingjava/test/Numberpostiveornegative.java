fb9e17e2708e7dafc9b500b064061dc336c36e75
jaga7babu <babujagapathi7@gmail.com>
2022-03-01 22:59:47 +0530

    commenting the new file to test

---
+++ src/llearingjava/test/Numberpostiveornegative.java
@@ -0,0 +1,22 @@
+package llearingjava.test;
+
+public class Numberpostiveornegative {
+
+	public static void main(String[] args) {
+     
+		   int a= 10;
+		   
+		   if (a<0)
+		   
+			   System.out.println(a+"the number is postive");
+		   
+		   else if (a>0) 
+		   
+		   System.out.println(a+"the number is negative");
+		   else 
+			   
+			   System.out.println(a+"the number is zero");
+
+	}
+
+}
